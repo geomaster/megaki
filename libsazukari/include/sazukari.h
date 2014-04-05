0a849fdc75e64b4cc15e18cbbafce54c6305bd92
David Davidović <geosoft.corp@gmail.com>
2014-04-06 00:14:47 +0200

    Define new Sazukari API
    I have amended Sazukari files to include a very different API approach. Instead
    of operating on a state machine and being notified of inbound data (like Yami),
    Sazukari now takes recv()- and send()-like callbacks and has big methods like
    szkr_do_handshake which run the full procedure and bail out if something goes
    wrong. This way, the state is implicitly the context inside the function: much
    easier to maintain. NB that this does not impair usability or trashes something
    important in favor of simplicity: since Sazukari is a client library, it makes
    the most sense to block on I/O (we can run this from a separate thread on the
    device, for example) as we will *absolutely* never need mass client
    connections. No actual library code has been written, tho: the changes are only
    in the headers so far.

--- libsazukari/include/sazukari.h
+++ libsazukari/include/sazukari.h
@@ -4,52 +4,39 @@
 #define SAZUKARI_MIN_BUFSIZE            8448
 
 /** Sazukari public declarations **/
-enum szkr_error {
-  szkr_error_none,
-  szkr_error_unknown,
-  szkr_incompatible_version,
-  szkr_service_unavailable,
-  szkr_blacklist_server,
-  szkr_buffer_too_short
-}
-
 typedef struct szkr_ctx_t szkr_ctx_t;
-typedef struct szkr_resp_t {
-  union {
-    /** Used if status is szkr_status_handshaking **/
-    length_t new_expected_size;
-    
-    /** Used if status becomes szkr_status_ready **/
-    length_t message_header_length;
-  }          length_param;
-  union {
-    /** Used if status is szkr_status_handhsaking **/
-    length_t send_size;
-    
-    /** Used if status is szkr_status_ready **/
-    length_t returned_message_size;
-  }          retlen_param;
-  szkr_error err_code;
-  
-  enum szkr_status {
-    szkr_status_handshaking,
-    szkr_status_ready,
-    szkr_status_error
-  } status;
-} szkr_resp_t;
 
-/** Sazukari API declarations **/
-length_t    szkr_get_ctx_size();
-int         szkr_bootstrap();
-int         szkr_new_ctx(szkr_ctx_t* ctx);
-szkr_resp_t szkr_start_connection(szkr_ctx_t*, byte* out_buffer, length_t buflen);
-szkr_resp_t szkr_incoming(szkr_ctx_t* ctx, byte* buffer, length_t len);
-szkr_resp_t szkr_compile_message(szkr_ctx_t* ctx, const byte* msg, length_t msglen,
-                                 byte* out_buffer, length_t buflen);
-length_t    szkr_deduce_expected_length(szkr_ctx_t* ctx, const byte* hdr); 
-int         szkr_reset_state(szkr_ctx_t* ctx);
-int         skzr_destroy();
-/** End Sazukari API declarations **/
+enum szkr_err_t {
+  szkr_err_none,
+  szkr_err_unknown,
+  szkr_err_incompatible_versions,
+  szkr_err_service_unavailable,
+  skzr_err_invalid_state,
+  szkr_err_protocol,
+  szkr_err_server_blacklisted
+};
+
+typedef (slength_t)(* szkr_read_cb)(byte*, length_t, void*);
+typedef (slength_t)(* szkr_write_cb)(byte*, length_t, void*);
+
+typedef struct szkr_iostream_t {
+  szkr_read_cb read_callback;
+  szkr_write_cb write_callback;
+  void* cb_param;
+} szkr_iostream_t;
 
 /** End Sazukari public declarations **/
+
+/** Sazukari public API **/
+int        szkr_init();
+int        szkr_new_ctx(szkr_ctx_t* ctx, szkr_iostream_t ios);
+int        szkr_do_handshake(szkr_ctx_t* ctx);
+int        szkr_redo_handshake(szkr_ctx_t* ctx);
+szkr_err_t szkr_last_error(szkr_ctx_t* ctx);
+int        szkr_send_message(szkr_ctx_t*, const byte* msg, length_t msglen, 
+                             byte* responsebuf, length_t* responselen);
+void       szkr_destroy_ctx(szkr_ctx_t* ctx);
+int        szkr_destroy();
+/** End Sazukari public API **/
+
 #endif // __SAZUKARI_H__
