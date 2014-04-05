cce44e420c94e039181283fc24f07afdd729cfdf
David Davidović <geosoft.corp@gmail.com>
2014-04-06 00:48:35 +0200

    Started work on Sazukari API implementation
    All public functions now have at least a minimal empty implementation and the
    code compiles. (I had to make some minor changes to the Makefile.) Sazukari
    context is fully defined and so are init and destroy functions. Nothing else
    works, tho, and I am working on it.

--- libsazukari/include/sazukari.h
+++ libsazukari/include/sazukari.h
@@ -6,18 +6,19 @@
 /** Sazukari public declarations **/
 typedef struct szkr_ctx_t szkr_ctx_t;
 
-enum szkr_err_t {
+typedef enum szkr_err_t {
   szkr_err_none,
   szkr_err_unknown,
   szkr_err_incompatible_versions,
   szkr_err_service_unavailable,
   skzr_err_invalid_state,
   szkr_err_protocol,
+  szkr_err_rehandshake_needed,
   szkr_err_server_blacklisted
-};
+} szkr_err_t;
 
-typedef (slength_t)(* szkr_read_cb)(byte*, length_t, void*);
-typedef (slength_t)(* szkr_write_cb)(byte*, length_t, void*);
+typedef slength_t (* szkr_read_cb)(byte*, length_t, void*);
+typedef slength_t (* szkr_write_cb)(byte*, length_t, void*);
 
 typedef struct szkr_iostream_t {
   szkr_read_cb read_callback;
@@ -29,6 +30,7 @@ typedef struct szkr_iostream_t {
 
 /** Sazukari public API **/
 int        szkr_init();
+int        szkr_get_ctxsize();
 int        szkr_new_ctx(szkr_ctx_t* ctx, szkr_iostream_t ios);
 int        szkr_do_handshake(szkr_ctx_t* ctx);
 int        szkr_redo_handshake(szkr_ctx_t* ctx);
@@ -36,7 +38,7 @@ szkr_err_t szkr_last_error(szkr_ctx_t* ctx);
 int        szkr_send_message(szkr_ctx_t*, const byte* msg, length_t msglen, 
                              byte* responsebuf, length_t* responselen);
 void       szkr_destroy_ctx(szkr_ctx_t* ctx);
-int        szkr_destroy();
+void       szkr_destroy();
 /** End Sazukari public API **/
 
 #endif // __SAZUKARI_H__
--- libsazukari/src/sazukari.c
+++ libsazukari/src/sazukari.c
@@ -2,6 +2,7 @@
 #include "megaki.h"
 #include <string.h>
 #include <openssl/rsa.h>
+#include <openssl/err.h>
 #include <openssl/bn.h>
 #include <openssl/sha.h>
 #include <openssl/rand.h>
@@ -10,37 +11,38 @@
 
 /** Definitions of Sazukari objects **/
 typedef struct szkr_ctx_t {
-  mgk_token_t   token;
-  mgk_aes_key_t server_symmetric,
-                master_symmetric;
-  RSA*          client_rsa;
+  mgk_token_t     token;
+  mgk_aes_key_t   server_symmetric,
+                  master_symmetric;
+  RSA*            client_rsa;
+  AES_KEY         kenc, kdec;
+  szkr_iostream_t ios;
+  szkr_err_t      last_err;
+  
   enum szkrctx_state {
     state_inactive,
-    state_awaiting_synack,
-    state_ready,
-    state_awaiting_ackack,
-    state_error
-  }             state;
-    
+    state_error,
+    state_ready
+  } state;
 } szkr_ctx_t;
 /** End definitions of Sazukari objects **/
 
 /** Sazukari internal functions **/
-int assemble_syn(szkr_resp_t* resp, szkr_ctx_t* ctx, byte* outbuf);
-int handle_synack(szkr_resp_t* resp, szkr_ctx_t* ctx, byte* iobuf, length_t len);
+int assemble_syn(szkr_ctx_t* ctx, byte* outbuf);
+int handle_synack(szkr_ctx_t* ctx, byte* inbuf, length_t len);
 int check_ackack(byte* buf, length_t len);
-int assemble_ack(szkr_resp_t* resp, szkr_ctx_t* ctx, byte* outbuf);
+int assemble_ack(szkr_ctx_t* ctx, byte* outbuf);
 int decode_msg(szkr_ctx_t* ctx, const byte* imsg, length_t len, byte* obuf, length_t *olen);
-int assemble_msg(szkr_resp_t* resp, szkr_ctx_t* ctx, const byte* inmsg, byte* outresp, length_t* resplen);
+int assemble_msg(szkr_ctx_t* ctx, const byte* inmsg, byte* outresp, length_t* resplen);
 /** End Sazukari internal functions **/
 
 /** Sazukari public API **/
-length_t szkr_get_ctx_size()
+int szkr_get_ctxsize()
 {
   return sizeof( szkr_ctx_t );
 }
 
-int szkr_bootstrap()
+int szkr_init()
 {
   ERR_load_crypto_strings();
   OpenSSL_add_all_ciphers();
@@ -48,67 +50,46 @@ int szkr_bootstrap()
   return(1);
 }
 
-int szkr_new_ctx(szkr_ctx_t* ctx)
+int szkr_new_ctx(szkr_ctx_t* ctx, szkr_iostream_t ios)
 {
-  ctx->state = state_awaiting_synacks
+  ctx->state = state_inactive;
+  ctx->ios = ios; 
+  ctx->client_rsa = NULL;
+  ctx->last_err = szkr_err_none;
   
   return(1);
 }
 
-szkr_resp_t szkr_incoming(szkr_ctx_t* ctx, byte* buffer, length_t len)
+szkr_err_t szkr_last_error(szkr_ctx_t* ctx)
 {
-  szkr_resp_t res;
-  res.status = szkr_status_ready;
-  res.err_code = szkr_error_none;
-  
-  if (length < sizeof(mgk_header_t))
-    goto failure;
-  
-  mgk_header_t* hdr = (mgk_header_t*) buffer;
-  byte msgbuf[ SAZUKARI_MIN_BUFSIZE ];
-  length_t buflen = SAZUKARI_MIN_BUFSIZE;
-  
-  switch (hdr->type) {
-    case magic_synack:
-      if (ctx->state == state_awaiting_synack)
-        handle_synack(&res, ctx, buffer, len);
-      else goto failure;
-      
-      break;
-   
-    case magic_msg:
-      if (ctx->state == state_ready) {
-        if (decode_msg(ctx, buffer, len, msgbuf, &buflen)) {
-          memcpy(buffer, msgbuf, buflen);
-          res.retlen_param.returned_message_length = buflen;
-        } else {
-          goto failure;
-        }
-      } else goto failure;
-      
-      break;
-      
-    default:
-      goto failure;
-  }
-  
-  return( res );
-  
-failure:
-  ctx->state = state_error;
-  res.status = szkr_status_error;
-  return( res );
+  return( ctx->last_err );
 }
 
-szkr_resp_t szkr_compile_message(szkr_ctx_t* ctx, const byte* msg, length_t msglen,
-                                 byte* out_buffer, length_t buflen)
+int szkr_do_handshake(szkr_ctx_t* ctx)
 {
-  
+  return(-1);
 }
 
-int szkr_reset_state(szkr_ctx_t* ctx)
+int szkr_redo_handshake(szkr_ctx_t* ctx)
 {
-  ctx->state = state_awaiting_synack;
-  
-  return(1);
+  return(-1);
+}
+
+int szkr_send_messge(szkr_ctx_t* ctx, const byte* msg, length_t msglen,
+                     byte* responsebuf, length_t* responselen)
+{
+  return(-1);
+}
+
+void szkr_destroy_ctx(szkr_ctx_t* ctx)
+{
+  if (ctx->client_rsa)
+    RSA_free(ctx->client_rsa);
+
+}
+
+void szkr_destroy()
+{
+  EVP_cleanup();
+  ERR_free_strings();
 }
