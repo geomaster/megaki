00fb5de0c441c6dd1f77ba4a4bc06625b241a021
David Davidović <geosoft.corp@gmail.com>
2014-04-05 02:33:01 +0200

    Importing changes to headers into Sazukari
    Recent Megaki header changes need to be imported here to keep the system
    up-to-date.

--- libsazukari/include/common.h
+++ libsazukari/include/common.h
@@ -1,6 +1,22 @@
 #ifndef __MEGAKI_COMMON_H__
 #define __MEGAKI_COMMON_H__
 #include <stdint.h>
+#ifdef DEBUG
+#include <time.h>
+#endif
+
+/** Common defs for Megaki protocol **/
+#define MEGAKI_RSA_KEYSIZE                2048
+#define MEGAKI_RSA_EXPSIZE                32
+#define MEGAKI_MAGIC_BYTES                5
+#define MEGAKI_PACKET_TYPE_BYTES          1
+#define MEGAKI_AES_KEYSIZE                256
+#define MEGAKI_AES_BLOCK_BYTES            16
+#define MEGAKI_TOKEN_BYTES                16
+#define MEGAKI_ERROR_CODE_BYTES           MEGAKI_AES_KEYBYTES
+#define MEGAKI_HASH_BYTES                 32
+#define MEGAKI_VERSION_BYTES              2
+#define MEGAKI_LENGTH_BYTES               4
 
 typedef uint8_t byte;
 typedef uint32_t length_t;
@@ -15,5 +31,35 @@ typedef enum loglevel {
   LOG_FATAL,
   LOG_QUIET
 } loglevel;
+
+#define MEGAKI_TIMEFMT "%d/%b/%Y:%H:%M:%S %z"
+#define MEGAKI_TIMEBUF  30
+#ifdef DEBUG
+#define MEGAKI_FMTIME(s) \
+  {   \
+    struct tm ts; \
+    time_t times = time(NULL); \
+    localtime_r(&times, &ts); \
+    strftime((s), sizeof((s)), MEGAKI_TIMEFMT, &ts); \
+  }
+  
+#define MEGAKI_LOGS(file, comp, str) \
+  { \
+    char s[MEGAKI_TIMEBUF]; \
+    MEGAKI_FMTIME(s); \
+    fprintf((file), "[" comp " %s] %s\n", s, str); \
+  } 
+  
+#define MEGAKI_LOGF(file, comp, fmt, ...) \
+  { \
+    char s[MEGAKI_TIMEBUF]; \
+    MEGAKI_FMTIME(s); \
+    fprintf((file), "[" comp " %s] " fmt "\n", s, __VA_ARGS__); \
+  } 
+#else
+#define MEGAKI_FMTIME(s)
+#define MEGAKI_LOGS(file, comp, str)
+#define MEGAKI_LOGF(file, comp, fmt, ...)
+#endif
 
 #endif
--- libsazukari/include/megaki.h
+++ libsazukari/include/megaki.h
@@ -3,19 +3,6 @@
 #include "common.h"
 #include <stdint.h>
 
-/** Common defs for Megaki protocol **/
-#define MEGAKI_RSA_KEYSIZE                2048
-#define MEGAKI_RSA_EXPSIZE                32
-#define MEGAKI_MAGIC_BYTES                5
-#define MEGAKI_PACKET_TYPE_BYTES          1
-#define MEGAKI_AES_KEYSIZE                256
-#define MEGAKI_AES_BLOCK_BYTES            16
-#define MEGAKI_TOKEN_BYTES                16
-#define MEGAKI_ERROR_CODE_BYTES           MEGAKI_AES_KEYBYTES
-#define MEGAKI_HASH_BYTES                 32
-#define MEGAKI_VERSION_BYTES              2
-#define MEGAKI_LENGTH_BYTES               4
-
 #define MEGAKI_AES_KEYBYTES               (MEGAKI_AES_KEYSIZE / 8)
 #define MEGAKI_RSA_EXPBYTES               (MEGAKI_RSA_EXPSIZE / 8)
 #define MEGAKI_RSA_KEYBYTES               (MEGAKI_RSA_KEYSIZE / 8)
