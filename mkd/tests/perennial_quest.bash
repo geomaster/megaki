5d7d1674699ae05a257014ff5343e42c28785dc0
David Davidović <geosoft.corp@gmail.com>
2014-04-10 23:56:57 +0200

    Minor typos fixed

--- mkd/tests/perennial_quest.bash
+++ mkd/tests/perennial_quest.bash
@@ -6,14 +6,14 @@ do
   # HALT
 	for ((i=0;i<2500;++i))
 	do
-    nc 127.0.0.1 6363 < $(DIR)../data/example-syn.bin > /dev/null &
+    nc 127.0.0.1 6363 < $(DIR)/../data/example-syn.bin > /dev/null &
 	done
 	sleep 5
 
   # HAMMERZEIT!
 	for ((i=0;i<30;++i))
 	do
-    LD_LIBRARY_PATH=$(DIR)../../libsazukari/bin $(DIR)../../libsazukari/tests/playground &
+    LD_LIBRARY_PATH=$(DIR)/../../libsazukari/bin $(DIR)/../../libsazukari/tests/playground &
 	done
 
 	sleep 20
