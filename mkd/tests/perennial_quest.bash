9e1093c6823b39f091c06a238e5d070c726df2d5
David Davidović <geosoft.corp@gmail.com>
2014-04-10 23:51:47 +0200

    Add simple stress test
    perennial_quest.sh will fire some requests towards a server on 127.0.0.1
    at port 6363, both precomputed requests from ../data/example-syn.bin and
    it will additionally run the playground test (not stable and will be
    changed later) to mix some chance and get any race conditions and bugs
    to show up in yami/yugi early.

---
+++ mkd/tests/perennial_quest.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+while :
+do
+  # HALT
+	for ((i=0;i<2500;++i))
+	do
+    nc 127.0.0.1 6363 < $(DIR)../data/example-syn.bin > /dev/null &
+	done
+	sleep 5
+
+  # HAMMERZEIT!
+	for ((i=0;i<30;++i))
+	do
+    LD_LIBRARY_PATH=$(DIR)../../libsazukari/bin $(DIR)../../libsazukari/tests/playground &
+	done
+
+	sleep 20
+	killall playground
+done
