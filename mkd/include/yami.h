5f27155368d7701459ca37e3f1ed324e8a1a1ecd
David Davidović <geosoft.corp@gmail.com>
2014-04-18 15:06:04 +0200

    Typo

--- mkd/include/yami.h
+++ mkd/include/yami.h
@@ -11,7 +11,7 @@
 typedef struct yami_conf_t {
   /** Configuration options for MKD Yami **/
   
-   /*** Server certificate raw data and size ***/
+  /*** Server certificate raw data and size ***/
   byte*         certificate_buffer;
   length_t      certificate_size;
   
